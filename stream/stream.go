c0db0291b069ff40a1f2a14cc1c45c4a54f744e7
Chieh <email@zzj.cool>
2022-03-09 22:47:16 +0800

    feat(stream): add BufSwapWithContext

--- stream/stream.go
+++ stream/stream.go
@@ -1,6 +1,7 @@
 package stream
 
 import (
+	"bufio"
 	"context"
 	"io"
 )
@@ -23,6 +24,29 @@ func SwapWithContext(ctx context.Context, up io.ReadWriter, dn io.ReadWriter) (e
 	}()
 	go func() {
 		_, err = io.Copy(dn, up)
+		done <- true
+	}()
+	<-done
+	return
+}
+
+func BufSwapWithContext(ctx context.Context, up io.ReadWriter, dn io.ReadWriter) (err error) {
+	upr := bufio.NewReader(up)
+	upw := bufio.NewWriter(up)
+	dnr := bufio.NewReader(dn)
+	dnw := bufio.NewWriter(dn)
+	done := make(chan bool, 1)
+	go func() {
+		<-ctx.Done()
+		done <- true
+	}()
+	go func() {
+
+		_, err = io.Copy(upw, dnr)
+		done <- true
+	}()
+	go func() {
+		_, err = io.Copy(dnw, upr)
 		done <- true
 	}()
 	<-done
